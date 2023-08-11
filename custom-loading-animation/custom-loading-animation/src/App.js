75a77a9c530911546bd8cbbea7b7a1bdee1f40e9
JaeWon Yun <alexyun0429@gmail.com>
2023-08-11 10:35:04 +1000

    fancy input

--- custom-loading-animation/custom-loading-animation/src/App.js
+++ custom-loading-animation/custom-loading-animation/src/App.js
@@ -1,7 +1,60 @@
 import "./App.css";
 
+// function App() {
+//   return <div className="loader"></div>;
+// }
+import React, { useState } from "react";
+import DeckGL from "@deck.gl/react";
+import { TerrainLayer } from "@deck.gl/geo-layers";
+
+const ELEVATION_DECODER = {
+  rScaler: 2,
+  gScaler: 0,
+  bScaler: 0,
+  offset: -10000,
+};
+
+const INITIAL_VIEW_STATE = {
+  latitude: 46.24,
+  longitude: -122.18,
+  zoom: 11.5,
+  bearing: 140,
+  pitch: 60,
+};
+
 function App() {
-  return <div className="loader"></div>;
+  const [elevation, setElevation] = useState(null);
+
+  const layer = new TerrainLayer({
+    id: "terrain",
+    minZoom: 0,
+    maxZoom: 23,
+    strategy: "no-overlap",
+    elevationDecoder: ELEVATION_DECODER,
+    elevationData:
+      "https://raw.githubusercontent.com/visgl/deck.gl-data/master/website/terrain.png",
+    texture:
+      "https://raw.githubusercontent.com/visgl/deck.gl-data/master/website/terrain-mask.png",
+    bounds: [-122.5233, 37.6493, -122.3566, 37.8159],
+  });
+
+  return (
+    <>
+      <DeckGL
+        initialViewState={INITIAL_VIEW_STATE}
+        controller={true}
+        layers={[layer]}
+        onHover={({ object }) => {
+          if (object) {
+            setElevation(object.position[2]);
+          } else {
+            setElevation(null);
+          }
+        }}
+      />
+      <div>Elevation: {elevation}</div>
+    </>
+  );
 }
 
 export default App;
---
+++ fancy-input/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <div className="input-group">
+        <input placeholder="..." className="input-group-input" />
+        <div className="input-group-line"></div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
